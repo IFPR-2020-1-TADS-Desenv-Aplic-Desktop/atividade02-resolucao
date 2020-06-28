cb7628c39206a26c73b8f8eb25fca642d2a12f36
Diego Stiehl <seccomiro@gmail.com>
2020-06-28 20:04:35 -0300

    Atualiza lista de arquivos para usar Bootstrap

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,30 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 
-// console.log('Hello from BrowserWindow');
+const form = document.querySelector("form");
+const input = document.querySelector("#path");
+const content = document.querySelector(".content");
 
-// const fileDiv = document.querySelector('.file');
-
-// fs.readFile('/home/diego/test.txt', 'utf-8', (err, data) => {
-//   fileDiv.innerHTML = data;
-// });
-
-const form = document.querySelector('form');
-const input = document.querySelector('#path');
-const content = document.querySelector('.content');
-
-form.addEventListener('submit', async e => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const path = input.value;
 
-  content.innerHTML = '';
+  content.innerHTML = "";
 
   try {
     const dir = await fs.promises.opendir(path);
     for await (const dirent of dir) {
-      content.innerHTML += `<li>${dirent.name}</li>`;
+      const dir = dirent.isDirectory();
+
+      content.innerHTML += `<li class="list-group-item ${
+        dir ? "bg-info item-folder" : ""
+      }">${dirent.name}</li>`;
     }
   } catch (error) {
-    content.innerHTML = '<li>Error loading files!</li>';
+    content.innerHTML =
+      '<li class="list-group-item bg-danger">Error loading files!</li>';
   }
 });
