f23bd95bdba1f89c938af4807fc7ef8313ab26b1
Diego Stiehl <seccomiro@gmail.com>
2020-06-28 20:56:28 -0300

    Adiciona "Salvar" e conclui exemplo

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,19 +1,34 @@
 const fs = require('fs');
 
-const form = document.querySelector('form');
+const form = document.querySelector('.form-path');
 const input = document.querySelector('#path');
 const content = document.querySelector('.content');
+const createFilePanel = document.querySelector('.create-file-panel');
+const formCreate = document.querySelector('.form-create');
+const createButton = document.querySelector('.btn-create');
+const fileNameInput = document.querySelector('#filename');
+const fileContentInput = document.querySelector('#file-content');
+
+createFilePanel.classList.add('hide');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  loadFiles(input.value);
+});
 
-  const path = input.value;
+createButton.addEventListener('click', e => {
+  createFilePanel.classList.remove('hide');
+});
 
-  loadFiles(path);
+formCreate.addEventListener('submit', e => {
+  e.preventDefault();
+  saveFile(input.value, fileNameInput.value, fileContentInput.value);
+  createFilePanel.classList.add('hide');
 });
 
 const loadFiles = async path => {
   content.innerHTML = '';
+  createFilePanel.classList.add('hide');
   try {
     const dir = await fs.promises.opendir(path);
     for await (const dirent of dir) {
@@ -31,6 +46,8 @@ const loadFiles = async path => {
       content.appendChild(li);
     }
   } catch (error) {
+    console.log(error);
+
     content.innerHTML =
       '<li class="list-group-item bg-danger">Error loading files!</li>';
   }
@@ -40,4 +57,10 @@ const selectEntry = entryName => {
   const newPath = `${input.value}/${entryName}`;
   input.value = newPath;
   loadFiles(newPath);
+  createFilePanel.classList.add('hide');
+};
+
+const saveFile = async (folder, filename, content) => {
+  await fs.promises.writeFile(`${folder}/${filename}`, content, 'utf-8');
+  loadFiles(folder);
 };
